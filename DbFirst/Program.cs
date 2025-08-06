86f370a1b3cf00920cc4794c0b217fc76f569aa4
david-vargas98 <edgar981125@live.com.mx>
2025-08-05 21:20:02 -0600

    feat: EFv6.0.0. | Creating table | Adding, modifying and deleteing columns | Delete table | Stored Procedures | Using Stored Procedures..
    EF was installed.
    
    Adding new entity/table by updating the DB model.
    
    Table UserProfiles | Adding column - The Name column was added to UserProfiles entity.
    
    Table Courses | Updating column - The Price column was renamed to FullPrice
    
    Table Courses | Deleting column - The LevelString column was deleted
    
    Table Courses | Updating column - The data type of Level column was changed from tinyint to smallint.
    
    Deleting UserProfile table.
    
    Importing store procedures by updating the model from the DB, as we did with the tables an columns.
    
    Making use of the stored procedures by writing a simple program which displays each of them.
    
    I WAS AMMENDING THE COMMITS THIS WHOLE TIME XDDD BUT LUCKILY I NOTICED ON TIME.

---
+++ DbFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext =  new PlutoDbContext();
+
+            var courses = dbContext.GetCourses();
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"Course: {course.Title}");
+            }
+        }
+    }
+}
